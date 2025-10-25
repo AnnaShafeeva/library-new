a7f4d01a8e69c5583432c85f57f762c5f4af9bab
Anna Sh <dzenborg@gmail.com>
2025-10-25 15:27:41 +0400

    initial commit

--- src/interfaces/IBook.ts
+++ src/interfaces/IBook.ts
@@ -1,10 +1,10 @@
-import { Document } from 'mongoose';
-
-export interface IBook extends Document {
+export interface IBook {
         title: string;
+        author: string;
         description: string;
-        authors?: string;
-        favorite?: string;
-        fileCover?: string;
-        fileName?: string;
+        favorite: boolean;
+        fileCover: string;
+        fileName: string;
+        bookId: string;
 }
+
